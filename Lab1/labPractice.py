9837f1045e6470606d076860fc7d643b38ce953f
safi-io <m.safi.ullah@outlook.com>
2025-02-26 14:16:11 +0500

    Basic Introduction of Python!

---
+++ Lab1/labPractice.py
@@ -0,0 +1,9 @@
+a= int(input("Enter a Number:\n"))
+b= int(input("Enter another Number:\n"))
+
+total_sum = a +b
+
+print("The sum is", total_sum)
+
+square_side = int(input("Enter side of square:\n"))
+print("Area of square is:", square_side*square_side)
